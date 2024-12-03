fb1b4d24854be142f8e67194d787b020684d4ddd
Nikita Dzhezhela <nikita.dzhezhela@nure.ua>
2024-12-03 18:41:22 +0200

    Adjusted navigation icons

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from 'expo-router';
+import { Bell, LayoutDashboard, User } from 'lucide-react-native';
 import React from 'react';
 import { Platform } from 'react-native';
 
@@ -10,27 +11,34 @@ export default function TabLayout() {
         tabBarStyle: Platform.select({
           ios: {
             position: 'absolute',
+            backgroundColor: '#F8F8FB',
           },
           default: {},
         }),
+        tabBarActiveTintColor: '#FD633D',
       }}
     >
       <Tabs.Screen
         name="notifications"
         options={{
           title: 'Notifications',
+          tabBarIcon: ({ color }) => <Bell color={color} size={20} />,
         }}
       />
       <Tabs.Screen
         name="index"
         options={{
           title: 'Dashboard',
+          tabBarIcon: ({ color }) => (
+            <LayoutDashboard color={color} size={20} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
+          tabBarIcon: ({ color }) => <User color={color} size={20} />,
         }}
       />
     </Tabs>
